d8c1b981e8eb4d2ddfafb1b41b5304d917fc502a
oke6 <ozaki.0828@gmail.com>
2021-07-22 06:52:01 +0900

    add abc210 c

--- abc210/c/main.py
+++ abc210/c/main.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python3
-s = input()
-n = int(input()) 
-a, b, c = input().split()
+n, k = map(int, input().split())
 list = list(map(int, input().split()))
-h, n = map(int, input().split())
+
+tempList = list[0:k]
+count = len(set(tempList))
+result = count
+
+colors = {}
+for color in tempList:
+  if color in colors:
+    colors[color] += 1
+  else:
+    colors[color] = 1
+
+
+for j in range(n-k):
+  tmpDel = list[j]
+  tmpAdd = list[j+k]
+
+  if (colors[tmpDel] == 1):
+    del colors[tmpDel]
+    count -= 1
+  else:
+    colors[tmpDel] -= 1
+
+  if tmpAdd in colors:
+    colors[tmpAdd] += 1
+  else:
+    colors[tmpAdd] = 1
+    count += 1
+
+  result = max(result, count)
+
+print(result)
